b847805b8624ff542e97591373e74031fb628e0e
alexssantos <arkanjo.alex@gmail.com>
2022-06-01 16:54:16 -0300

    Componente Login

--- src/ClientApp/ngapp/src/app/app.module.ts
+++ src/ClientApp/ngapp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInUpComponent } from './pages/sign-in-up/sign-in-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInUpComponent
   ],
   imports: [
     BrowserModule,
